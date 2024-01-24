6f78907e1a0454207e29dd72c753c92d4f43ed29
ChillingTime <102903069+NotForGetNow@users.noreply.github.com>
2024-01-24 22:52:49 +0600

    Add files via upload

---
+++ Домашка/js.js
@@ -0,0 +1,65 @@
+// #1
+let name = prompt("Как тебя зовут?")
+
+alert("Привет, его имя - " + name)
+
+
+// #2
+let old = +prompt("В каком году ты родился?")
+const year = 2024
+
+let resultsYear = old - 2024
+
+alert("Тебе - " + -resultsYear + "?")
+
+
+// #3
+let perimeter = +prompt("Введите длинну стороны квадрата")
+let resultsPerimeter = perimeter * 4
+
+alert(resultsPerimeter)
+
+
+// #4
+let radius = +prompt("Введите радиус окружности")
+const pi = 3.14
+let resultsRadius = pi * (radius * radius)
+
+alert(resultsRadius)
+
+
+// #5
+let km = +prompt("Введите растояние между двух городов (км)")
+let t = +prompt("Введите время, за которое вы хотите дойти до города (час)")
+let resultsSpeed = km / t
+
+alert(resultsSpeed + " - скорость, с которой вам нужно двигаться, чтобы добаться вовремя")
+
+
+// #6
+let $ = +prompt("Введите сумму в долларах")
+const $Course = 0.92
+let resultsCourse = $ * $Course
+
+alert(resultsCourse + " - евро")
+// #7
+let gb = +prompt("Введите кол-во памяти в гб")
+const files = 0.82
+let resultsGb = gb * files
+
+alert(resultsGb + " - файлов")
+
+
+// #8
+let wallet = +prompt("Сколько денег у вас есть?")
+let choco = +prompt("Сколько стоит шоколад?")
+let resultsChoco = wallet / choco
+
+alert(resultsChoco)
+
+
+// #9
+
+
+
+// #10
